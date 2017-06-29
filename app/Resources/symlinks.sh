69d72690fb92bc98f3d9771bad0e9e25ac178922
Daniel Hornik <daniel.1302@gmail.com>
2017-06-29 07:59:53 +0200

    symlinks

---
+++ app/Resources/symlinks.sh
@@ -0,0 +1,7 @@
+ls -s ../../vendor/almasaeed2010/adminlte/ adminlte
+ls -s ../../src/AppBundle/Resources/public/ app
+ls -s  ../../vendor/twbs/bootstrap/ bootstrap 
+ls -s  ../../vendor/datatables/datatables/ datatables
+ls -s  /usr/share/nginx/html/articles/vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/public framework
+ls -s  ../../vendor/components/jquery jquery
+ls -s  ../../vendor/components/jqueryui jqueryui
